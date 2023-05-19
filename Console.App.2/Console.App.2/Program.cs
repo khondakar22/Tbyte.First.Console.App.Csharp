fad02861ed4832bacf4a3d80cb558a4d4b3c12f2
khondakar22 <khondakar22readul@gmail.com>
2023-05-19 19:18:12 +0200

    First Console app implemented

---
+++ Console.App.2/Console.App.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Console.App._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please Enter your name");
+            var takeName = System.Console.ReadLine();
+            System.Console.WriteLine("Hello " + takeName);
+            System.Console.WriteLine("_______________________");
+            System.Console.WriteLine("Please Enter your password");
+            var password = System.Console.ReadLine();
+            System.Console.WriteLine("Welcom " + password);
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ Frist.Console.App/Frist.Console.App/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Frist.Console.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+
+}
