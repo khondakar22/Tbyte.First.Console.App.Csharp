9549def23aaa8e342269f60d0b2603640bd35372
riyad786 <r.islam@id-ware.com>
2023-05-24 18:43:49 +0200

    Console Class Background Color and Built in method shown

---
+++ CsharpBasicsMoreTypes/ConsoleClassMethods/ConsoleClassMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleClassMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.Clear();
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("Hello, World!"); // Cursor move in the next line
+            //Console.Write("Display and puts the cursor in the same line");
+            Console.Write("Hello Universe!");
+            //Console.Read();
+           var userInput = Console.ReadLine();
+           Console.WriteLine(userInput);
+            Console.ReadKey();
+            //Console.Clear();
+            //Console.Read();
+        }
+    }
+}
